7d566c0f66e9b89a7dd757a32f52d82156b249c5
yy783 <1418764020@qq.com>
2020-10-15 13:58:11 +0800

    测试

--- 函数/指数函数.py
+++ 函数/指数函数.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 def f(t):
     return np.exp(-t)
 
+def g(t):
+    return np.exp(t)
+
+def h(t):
+    return np.exp(0*t)
 
 t1 = np.arange(-5.0, 5.0, 0.1)
 t2 = np.arange(-5.0, 5.0, 0.1)
 
 
-plt.figure(2)           
-plt.subplot(221)     
-plt.plot(t1, f(t1), 'b-')
-#设置坐标轴范围
+
+plt.plot(t1, f(t1), '-b',label='X(t)=e^-t')
+plt.plot(t2, g(t2), '-r',label='X(t)=e^t')
+plt.plot(t2, h(t2), 'g-',label='X(t)=e^0=1')
+plt.legend(loc='best')
+
 plt.xlim((-5, 5))
 plt.ylim((-1,10))
 
-#设置坐标轴名称
 plt.xlabel('t/s')
 plt.ylabel('X(t)')
-#设置坐标轴刻度
 ax = plt.gca()                                           
-
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')         
 ax.xaxis.set_ticks_position('bottom')
-ax.yaxis.set_ticks_position('left')          # 指定下边的边作为 x 轴   指定左边的边为 y 轴
-ax.spines['bottom'].set_position(('data', 0))   #指定 data  设置的bottom(也就是指定的x轴)绑定到y轴的0这个点上
+ax.yaxis.set_ticks_position('left')         
+ax.spines['bottom'].set_position(('data', 0))   
 ax.spines['left'].set_position(('data', 0))
-plt.title("Functional Equation:ω=0,σ=-1,X(t)=e^-t")
-
+plt.title("Functional Equation:")
 
 
 plt.show()
