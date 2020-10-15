218bff5170beaa5048924706687f0eab7451ec51
yy783 <1418764020@qq.com>
2020-10-15 14:28:10 +0800

    测试

--- 函数/正弦函数.py
+++ 函数/正弦函数.py
@@ -6,8 +6,16 @@ from pylab import *
 x = np.arange(-10.0, 10.0, 0.01)
 y1 = np.sin(x)
 
+plt.xlim((-5, 5))
+plt.ylim((-1,10))
+
+
 plt.figure(1)
 plt.subplot(211)
 plt.plot(x, y1)
+
+plt.title("x(t)=Acos(wt+ψ)")
+
+
 
 plt.show()
